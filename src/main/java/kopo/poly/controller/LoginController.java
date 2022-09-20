8a056698246375d3e40efb9ca8ae2a608f6ad64f
Choiyeojin <81405125+YEOJIN0305@users.noreply.github.com>
2022-09-20 15:30:12 +0900

    Y 220920

--- src/main/java/kopo/poly/controller/LoginController.java
+++ src/main/java/kopo/poly/controller/LoginController.java
@@ -22,4 +22,25 @@ public class LoginController {
         return "login/register";
     }
 
+    @GetMapping(value = "login/find-id")
+    public String findId() throws Exception {
+        log.info(this.getClass().getName()+".register Start!!");
+        log.info(this.getClass().getName()+".register End!!");
+        return "login/find-id";
+    }
+
+    @GetMapping(value = "login/find-pw")
+    public String findPw() throws Exception {
+        log.info(this.getClass().getName()+".register Start!!");
+        log.info(this.getClass().getName()+".register End!!");
+        return "login/find-pw";
+    }
+
+    @GetMapping(value = "login/forgot-password")
+    public String forgotPassword() throws Exception {
+        log.info(this.getClass().getName()+".register Start!!");
+        log.info(this.getClass().getName()+".register End!!");
+        return "login/forgot-password";
+    }
+
 }
