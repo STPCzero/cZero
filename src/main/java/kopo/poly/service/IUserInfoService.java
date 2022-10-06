5d61e30236452ae58e01685627711ac9f1e7dc18
Choiyeojin <81405125+YEOJIN0305@users.noreply.github.com>
2022-10-07 03:13:40 +0900

    Y 221007

--- src/main/java/kopo/poly/service/IUserInfoService.java
+++ src/main/java/kopo/poly/service/IUserInfoService.java
@@ -6,9 +6,6 @@ public interface IUserInfoService {
 
     int insertUserInfo(UserInfoDTO uDTO) throws Exception;
 
-    /* 아이디 비밀번호 일치하는지 확인 */
-    UserInfoDTO getUserLoginCheck(UserInfoDTO uDTO) throws Exception;
-
     UserInfoDTO findUserId(UserInfoDTO uDTO) throws Exception;
 
     int updateUserPw(UserInfoDTO pDTO) throws Exception;
