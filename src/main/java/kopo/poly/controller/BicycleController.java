1c5a88be90335588c1760aa67fec0133b868bb3c
Hyegyeong Joo <101082130+Hyegyeong25@users.noreply.github.com>
2022-10-26 16:26:22 +0900

    H 221026 공공자전거&카카오 API 적용중

--- src/main/java/kopo/poly/controller/BicycleController.java
+++ src/main/java/kopo/poly/controller/BicycleController.java
@@ -1,17 +1,26 @@
 package kopo.poly.controller;
 
+import kopo.poly.service.IBicycleService;
+import kopo.poly.service.IMailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
-@Controller
+@RequestMapping("bicycle")
+@RestController
 public class BicycleController {
 
-    @GetMapping("/bicycle/bicycle")
+    @Resource(name = "BicycleService")
+    private IBicycleService bicycleService;
+
+    @GetMapping("bicycle")
     public String bicycle(HttpServletRequest request, Model model) throws Exception {
         log.info(this.getClass().getName() + ".bicycle Start!!");
         log.info(this.getClass().getName() + ".bicycle End!!");
---
+++ src/main/java/kopo/poly/service/impl/BicycleService.java
@@ -0,0 +1,11 @@
+package kopo.poly.service.impl;
+
+import kopo.poly.service.IBicycleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service("BicycleService")
+public class BicycleService implements IBicycleService {
+
+}
