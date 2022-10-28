3b8c58579e3a806ec3ecf6fe4520c203a33a2c88
DreamFlower14 <sunghh1004@naver.com>
2022-10-28 21:03:18 +0900

    S 2201028 프론트 작업

--- src/main/java/kopo/poly/dto/NewsDTO.java
+++ src/main/java/kopo/poly/dto/NewsDTO.java
@@ -6,7 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class NewsDTO {
+    private String News_seq;
     private String News_title;
     private String News_Department;
     private String News_date;
+    private String News_url;
+    private String News_Check_Time;
 }
